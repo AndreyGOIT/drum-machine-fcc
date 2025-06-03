1d1f92b9b43f34a85f717b273f75d0b1ef0d932b
AndreyGOIT <ae.borga@gmail.com>
2025-06-03 12:34:25 +0300

    add container, display and drum-pads

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div id="drum-machine" className="App">
       <h1>Hello, React!</h1>
+      <div className="drum-pads">Q</div>
+      <div className="drum-pads">W</div>
+      <div className="drum-pads">E</div>
+      <div className="drum-pads">A</div>
+      <div className="drum-pads">S</div>
+      <div className="drum-pads">D</div>
+      <div className="drum-pads">Z</div>
+      <div className="drum-pads">X</div>
+      <div className="drum-pads">C</div>
+      <div id="display">Display</div>
     </div>
   );
 }
